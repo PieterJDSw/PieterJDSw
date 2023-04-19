94baea9bb1f095bb935b10d5657edb9d57b12926
Pieter <130872729+PieterJDSw@users.noreply.github.com>
2023-04-20 00:25:41 +0200

    added some scaffold

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
-
+import "bulma/css/bulma.min.css";
 
 function App() {
+  let backgroundColor = 'black';
+  let textcolor = 'white';
   return (
-    <div className="App">
-      <header className="App-header">
-    <h1>Welcome</h1>
-      </header>
-    </div>
+    <section className="max-width" style={{backgroundColor: backgroundColor}}>
+      <div className="container">
+   <h1 className="title" style={{color: textcolor}}> Hello My Name is <strong>Pieter Swanepoel</strong> </h1>
+   <p className="subtitle" style={{color: textcolor}}> I am A Full stack Software Engineer      </p>
+ </div>
+      <img src={process.env.PUBLIC_URL + '/backdrop.jpg'} /> 
+      <div className="container">
+   <h1 className="title"> Hello My Name is <strong>Pieter Swanepoel</strong> </h1>
+   <p className="subtitle"> I am A Full stack Software Engineer      </p>
+ </div>
+
+ 
+</section>
   );
 }
 
