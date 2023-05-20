c090d16839ea8f94c86890ee3e261c44513e2fe1
Pieter <130872729+PieterJDSw@users.noreply.github.com>
2023-05-21 00:54:30 +0200

    Changes

--- src/App.js
+++ src/App.js
@@ -14,18 +14,13 @@ function App() {
           <h1 class="title is-1 is-spaced" style={{color: textcolor}}> Hello My Name is <strong>Pieter Swanepoel</strong> </h1>
           <p class="subtitle is-3" style={{color: textcolor}}> I am A Full stack Software Engineer      </p>
           <ProfileImage/>
-          {/* <img src={process.env.PUBLIC_URL + '/backdrop.jpg'} /> */}
           <p class="title is-1 is-spaced" style={{color: textcolor}}> A Little About me  :   </p>
-
-
           <div class="columns">
           <div class="column">
           <Card message ={"I am married"} src={process.env.PUBLIC_URL + '/ons.jpg'}/> 
-
           </div>
           <div class="column">
           <Card message ={"Myself and my wife have two dogs"} src={process.env.PUBLIC_URL + '/ons.jpg'}/> 
-
           </div>
           <div class="column">
                 <Card message ={"My Family"} src={process.env.PUBLIC_URL + '/ons.jpg'}/>  
@@ -33,9 +28,8 @@ function App() {
           <div class="column">
                 <Card message ={"My Other Family"} src={process.env.PUBLIC_URL + '/ons.jpg'}/>  
           </div>
-  
-</div>
       </div>
+    </div>
              
   <ContentBody/>
 
