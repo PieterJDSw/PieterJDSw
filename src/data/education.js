16e5b551461bb9d2b6277015b6c1e2d68607060b
Pieter <130872729+PieterJDSw@users.noreply.github.com>
2023-05-21 01:06:44 +0200

    Update education.js
    changes to work experience

--- src/data/education.js
+++ src/data/education.js
@@ -1,19 +1,30 @@
 export const items = [{
-    title: "2016-2018",
-    cardTitle: "BSc Computer Science",
-    
+    title: "2019 ",
+    cardTitle: "Xtrapolate: Full Stack Software Engineer",
+    cardSubtitle: "Software Engineer, Where i worked on multiple project that included Technolgies such as Laravel PHP, Eloquent, Vue, and SQL Server",
   },{
-    title: "2019",
-    cardTitle: "BSc Honors Computer Science",
-    url: "http://www.history.com",
-    cardSubtitle:"Honors Research in IOT",
-    cardDetailedText: "Subjects: Data Warehouses, Databases, AI, and Decisiion Support Systems",
+    title: "2020 ",
+    cardTitle: "BBD Software: Software Engineering Consultant",
+    cardSubtitle: "Software Engineer",
+    items: [
+        { cardTitle: 'Vodacom',cardSubtitle: "Software Engineer" },
+        { cardTitle: 'Absa' ,cardSubtitle: "Software Engineer"},
+        { cardTitle: 'Absa' },
+        { cardTitle: 'Absa' },
+      ],
     
-  },{
-    title: "2020-2023",
-    cardTitle: "MSc Computer Science",
-    cardSubtitle:"DeepLearning Based Object Detection in Space Weather",
+  },
+//   {
+//     title: "2023",
+//     cardTitle: "Entelect: ",
+//     cardSubtitle:"Absa:Full Stack Engineer",
+//     cardDetailedText: "Subjects: Data Warehouses, Databases, AI, and Decisiion Support Systems",
+    
+//   },{
+//     title: "2020-2023",
+//     cardTitle: "MSc Computer Science",
+//     cardSubtitle:"DeepLearning Based Object Detection in Space Weather",
     
     
-  }
+//   }
 ];
