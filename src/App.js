95c5e3d3b769985ccff2655cfb61bd9fe96ff6e0
Pieter <130872729+PieterJDSw@users.noreply.github.com>
2023-05-21 21:35:38 +0200

    wording to skills

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div className="section has-text-centered">
         
           <h1 class="title is-1 is-spaced" style={{color: textcolor}}> Hello My Name is <strong>Pieter Swanepoel</strong> </h1>
-          <p class="subtitle is-3" style={{color: textcolor}}> I am A Full stack Software Engineer      </p>
+          <p class="subtitle is-3" style={{color: textcolor}}> I am A Software Engineer With a passion for Back-End     </p>
           
           <ProfileImage/>
           <p class="title is-1 is-spaced mt-5 mb-5" style={{color: textcolor}}> A Little About me  :   </p>
@@ -37,21 +37,28 @@ function App() {
           <figure class="image is-128x128 is-justify-content-center is-align-content-center is-inline-block">
           <img src={process.env.PUBLIC_URL + '/iconsjava.png'}/>
           </figure>   
+          <p>Java EE</p>  
+
         </div>
         <div class="column">
         <figure class="image is-128x128 is-justify-content-center is-align-content-center is-inline-block">
           <img src={process.env.PUBLIC_URL + '/iconspringboot.png'}/>
-          </figure>          
+          </figure>   
+          <p>Spring Boot</p>  
+       
         </div>
         <div class="column">
         <figure class="image is-128x128 is-justify-content-center is-align-content-center is-inline-block">
           <img src={process.env.PUBLIC_URL + '/hibernate.png'}/>
-          </figure>          
+          </figure>   
+          <p>Hibernate & JPA</p>       
         </div>
         <div class="column">
           <figure class="image is-128x128 is-justify-content-center is-align-content-center is-inline-block">
           <img src={process.env.PUBLIC_URL + '/iconsgolang.png'}/>
-          </figure>          
+          </figure>  
+          <p>Golang</p>       
+        
         </div>
         
       </div>
@@ -60,22 +67,26 @@ function App() {
         <div class="column">
           <figure class="image is-128x128 is-justify-content-center is-align-content-center is-inline-block">
           <img src={process.env.PUBLIC_URL + '/rest.png'}/>
-          </figure>   
+          </figure> 
+          <p>REST</p>
         </div>
         <div class="column">
           <figure class="image is-128x128 is-justify-content-center is-align-content-center is-inline-block">
           <img src={process.env.PUBLIC_URL + '/iconsnodejs.png'}/>
-          </figure>          
+          </figure>  
+          <p>Node Express</p>        
         </div>
         <div class="column ">
           <figure class="image is-128x128 is-justify-content-center is-align-content-center is-inline-block">
           <img src={process.env.PUBLIC_URL + '/iconsangular.png'}/>
-          </figure>          
+          </figure>  
+          <p>Angular</p>        
         </div>
         <div class="column">
           <figure class="image is-128x128 is-justify-content-center is-align-content-center is-inline-block">
           <img src={process.env.PUBLIC_URL + '/pl-sql.png'}/>
-          </figure>          
+          </figure>  
+          <p>Oracle PL-SQL</p>        
         </div>
         {/* <div class="column">
           <figure class="image is-128x128 is-justify-content-center is-align-content-center is-inline-block">
@@ -93,21 +104,25 @@ function App() {
           <figure class="image is-128x128 is-justify-content-center is-align-content-center is-inline-block">
           <img src={process.env.PUBLIC_URL + '/aws2.png'}/>
           </figure>   
+          <p>AWS</p>
         </div>
         <div class="column">
           <figure class="image is-128x128 is-justify-content-center is-align-content-center is-inline-block">
           <img src={process.env.PUBLIC_URL + '/jenkins2.png'}/>
-          </figure>          
+          </figure>   
+          <p>Jenkins</p>       
         </div>
         <div class="column ">
           <figure class="image is-128x128 is-justify-content-center is-align-content-center is-inline-block">
           <img src={process.env.PUBLIC_URL + '/rxjs.png'}/>
-          </figure>          
+          </figure>   
+          <p>RXJS</p>       
         </div>
         <div class="column">
           <figure class="image is-128x128 is-justify-content-center is-align-content-center is-inline-block">
           <img src={process.env.PUBLIC_URL + '/docker.png'}/>
-          </figure>          
+          </figure>   
+          <p>Docker</p>       
         </div>
         {/* <div class="column">
           <figure class="image is-128x128 is-justify-content-center is-align-content-center is-inline-block">
